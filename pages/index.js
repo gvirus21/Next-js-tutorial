f778766cfe75b371eafa8edce889bdd18c327206
gourav <gvirus21yt@gmail.com>
2022-03-07 23:03:37 +0530

    added client side routing with links

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,16 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import Link from "next/link";
 
 export default function Home() {
   return (
-    <h1>Home page</h1>
-  )
+    <>
+      <h1>Home page</h1>
+      <Link href="/blog">
+        <a>Blog</a>
+      </Link>
+
+      <Link href="/product">
+        <a>Product</a>
+      </Link>
+    </>
+  );
 }
